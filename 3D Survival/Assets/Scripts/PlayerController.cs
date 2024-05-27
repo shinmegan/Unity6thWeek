c015b2bd2dd6b5ab1bbefc04910526ddd06aaad5
이유신 <smdeks93@naver.com>
2024-05-27 11:01:52 +0900

    [feat] environment settings toggle
    1번을 누르면, 환경설정창이 화면에 표시됨.
    동시에 마우스 커서가 표시되고, 카메라 회전은 멈춤

--- 3D Survival/Assets/Scripts/PlayerController.cs
+++ 3D Survival/Assets/Scripts/PlayerController.cs
@@ -14,35 +14,45 @@ public class PlayerController : MonoBehaviour
     public float jumpPower;
 
     [Header("Look")]
-    public Transform cameraContainer;
-    public float minXLook;
-    public float maxXLook;
-    private float camCurXRot;
+    public Transform cameraContainer; // 회전할 오브젝트
+    public float minXLook; // 회전 최소값
+    public float maxXLook; // 회전 최대값
+    private float camCurXRot; // 현재 카메라 X축 회전 값
     public float lookSensitivity; // 카메라 회전 민감도
     private Vector2 mouseDelta;
 
     private Rigidbody _rigidbody;
+
+    [Header("Settings")]
+    public GameObject settingsMenu; // 설정창 GameObject
+
+    private bool isSettingsOpen = false; // 설정창 활성화 상태를 관리하는 변수
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Start is called before the first frame update
     void Start()
     {   // 게임 시작 시 마우스 커서를 숨기고 화면 중앙에 고정
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        Move();
+        if (!isSettingsOpen) // 설정창이 열려있지 않을 때만 이동 처리
+        {
+            Move();
+        }
     }
 
     private void LateUpdate()
     {
-        CameraLook();
+        if (!isSettingsOpen) // 설정창이 열려있지 않을 때만 카메라 회전 처리
+        {
+            CameraLook();
+        }
     }
 
     // 플레이어 이동 처리 메서드
@@ -95,7 +105,7 @@ public class PlayerController : MonoBehaviour
 
     // 땅에 붙어있는지 확인하는 메서드
     bool IsGrounded()
-    {
+    {   // 플레이어의 네 귀퉁이에서 아래로 향하는 4개의 레이캐스트 생성
         Ray[] rays = new Ray[4]
         {
             new Ray(transform.position + (transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
@@ -106,12 +116,35 @@ public class PlayerController : MonoBehaviour
 
         for(int i = 0; i < rays.Length; i++)
         {
-            if (Physics.Raycast(rays[i], 0.1f, groundLayerMask))
+            if (Physics.Raycast(rays[i], 0.4f, groundLayerMask))
             {
                 return true;
             }
         }
 
         return false;
+    }
+
+    // 키보드 1 입력을 받아오는 메서드
+    public void OnSetting(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Started && !isSettingsOpen) // 키를 눌렀을 때
+        {
+            // 설정창 띄우기
+            // 마우스 커서가 보이고 카메라 회전 막기
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            
+            isSettingsOpen = true;
+        }
+        else if (context.phase == InputActionPhase.Started && isSettingsOpen ) // 키를 한 번 더 눌렀을 때
+        {
+            // 설정창 감추기
+            // 마우스 커서 감추고 카메라 회전 시작
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            
+            isSettingsOpen = false;
+        }
     }
 }
